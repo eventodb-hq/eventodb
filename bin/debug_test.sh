09a3f2d6265770d58b76c03d23ce3ec076782442
Roman Heinrich <roman.heinrich@gmail.com>
2025-12-17 07:48:40 +0100

    Chore: remove crap

--- bin/debug_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Run from project root
-cd "$(dirname "$0")/.." || exit 1
-
-TEST_DB_MODE=file KEEP_NAMESPACES=true bun test --test-name-pattern "$1"
--- bin/restart_frontend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd kids-real-ui
-lsof -i :3555 | awk 'NR>1 {print $2}' | xargs kill -9
-bun run dev
